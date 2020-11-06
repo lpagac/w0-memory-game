1d6b5b8998fc44e46ddd919414db565a0011ff59
Tracy Jiang <tracyjiangmx@gmail.com>
2020-11-06 11:31:23 -0800

    update js

--- memory-game.js
+++ memory-game.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 // create elements function
 // first create a container div
 // add a blank div as the base class for each card (card back)
@@ -7,44 +7,41 @@
 // append container div to board
 
 // flip card func
-// check if the event target is a blank class div 
+// check if the event target is a blank class div
 // if true then toggle the class of the div to its sibling color class inside of the container div
-// 
+//
 
 // Set timeout will contain unflip func
 // unflip func
 // toggle the class of target element back to blank after one second
-// 
+//
 
 /** Memory game: find matching pairs of cards and flip both of them. */
 
 const FOUND_MATCH_WAIT_MSECS = 1000;
-const COLORS = [
-  "red", "blue", "green", "orange", "purple",
-  "red", "blue", "green", "orange", "purple",
-];
+const COLORS = [ 'red', 'blue', 'green', 'orange', 'purple', 'red', 'blue', 'green', 'orange', 'purple' ];
 
 const colors = shuffle(COLORS);
+let flippedCards = [];
 
 createCards(colors);
-
 
 /** Shuffle array items in-place and return shuffled array. */
 
 function shuffle(items) {
-  // This algorithm does a "perfect shuffle", where there won't be any
-  // statistical bias in the shuffle (many naive attempts to shuffle end up not
-  // be a fair shuffle). This is called the Fisher-Yates shuffle algorithm; if
-  // you're interested, you can learn about it, but it's not important.
+	// This algorithm does a "perfect shuffle", where there won't be any
+	// statistical bias in the shuffle (many naive attempts to shuffle end up not
+	// be a fair shuffle). This is called the Fisher-Yates shuffle algorithm; if
+	// you're interested, you can learn about it, but it's not important.
 
-  for (let i = items.length - 1; i > 0; i--) {
-    // generate a random index between 0 and i
-    let j = Math.floor(Math.random() * i);
-    // swap item at i <-> item at j
-    [items[i], items[j]] = [items[j], items[i]];
-  }
+	for (let i = items.length - 1; i > 0; i--) {
+		// generate a random index between 0 and i
+		let j = Math.floor(Math.random() * i);
+		// swap item at i <-> item at j
+		[ items[i], items[j] ] = [ items[j], items[i] ];
+	}
 
-  return items;
+	return items;
 }
 
 /** Create card for every color in colors (each will appear twice)
@@ -55,42 +52,64 @@ function shuffle(items) {
  */
 
 function createCards(colors) {
-  const gameBoard = document.getElementById("game");
+	const gameBoard = document.getElementById('game');
 
-  for (let color of colors) {
-    let container = document.createElement('div');
-    let blank = document.createElement('div');
-    let colored = document.createElement('div');
-    container.classList.add('cardContainer');
-    blank.classList.add('cards blankCard');
-    colored.classList.add(`cards ${color} hidden`);
-    container.append(blank, colored);
-    gameBoard.append(container);
-  }
+	for (let color of colors) {
+		let container = document.createElement('div');
+		let blank = document.createElement('div');
+		let colored = document.createElement('div');
+		container.classList.add('cardContainer');
+		blank.classList.add('cards blankCard');
+		colored.classList.add(`cards ${color} hidden`);
+		container.append(blank, colored);
+		gameBoard.append(container);
+	}
 }
 
 /** Flip a card face-up. */
 
-function flipCard(card) {
-  if(evt.target.classList.includes('blankCard')) {
-    evt.target.classList.toggle(`${evt.target.nextElementSibling.classList[0]} ${evt.target.nextElementSibling.classList[1]}`);
-  }
+function flipCard(evt) {
+	if (evt.target.classList.includes('blankCard')) {
+		evt.target.classList.toggle(
+			`${evt.target.nextElementSibling.classList[0]} ${evt.target.nextElementSibling.classList[1]}`
+		);
+	}
 }
 
 /** Flip a card face-down. */
 
-function unFlipCard(card) {
-  // ... you need to write this ...
+function unFlipCard(evt) {
+	evt.target.classList.toggle('blankCard cards');
 }
 
 /** Handle clicking on a card: this could be first-card or second-card. */
-// event listener for click and then flip card 
+// event listener for click and then flip card
 // push class color into global array
 // if array length is 2 check if match
 // if they are a match remove something to keep it from flipping
 // else clear array
 // settimeout and unflip after 1 sec
+
+//click event listener
+
+let children = [ ...gameBoard.children ];
+
+children.forEach((card) => {
+	card.addEventListener('click', handleCardClick);
+});
+
 function handleCardClick(evt) {
-  // ... you need to write this ...
-  
+	flipCard(evt);
+
+	flippedCards.push(evt.target.classList[1]);
+
+	if (flippedCards.length === 2) {
+		if (flippedCards[0] === flippedCards[1]) {
+			return;
+		} else {
+			flippedCards = [];
+		}
+	}
+
+	setTimeout(unFlipCard(evt), FOUND_MATCH_WAIT_MSECS);
 }
